bd6777ddf0ab3008068942c3baa67a0a4be8b68c
Jamie Piper <jamie-piper@hotmail.co.uk>
2020-02-22 12:16:03 +0000

    initial commit

--- src/redactor.js
+++ src/redactor.js
@@ -1,16 +1,18 @@
 let actionRedact = function() {
   const redactedOutput = '████';
-  let originalText = document.querySelector('.unredacted-text').value; // original content to be redacted
-  let contentToRedact = document.querySelector('.to-redact').value; // words to redact from original content
-  
+  let originalText = document.querySelector('.unredacted-container textarea').value; // original content to be redacted
+  let contentToRedact = document.querySelector('.to-redact'); // words to redact from original content
+  let contentToRedactValue = contentToRedact.value;
+  contentToRedact.focus();
+
   // prepare content for redacting
-  let redactList = contentToRedact.split(/[\W]+/);
+  let redactList = contentToRedactValue.split(/[\W]+/);
   let redactOriginalList = originalText.trim().split(/[\W]+/);
 
   // redact original content
   redactList.forEach(function(redactWord) {
     redactOriginalList.forEach(function(originalWord, i) {
-      if (originalWord && 
+      if (originalWord &&
           redactWord.trim().toLowerCase() === originalWord.toLowerCase()
          ) {
         redactOriginalList.splice(i, 1, redactedOutput);
@@ -20,11 +22,13 @@ let actionRedact = function() {
 
   // prepare and output redacted content
   let redactedText = redactOriginalList.join(' ');
-  document.querySelector('.redact-text p').innerText = redactedText;
-};
 
-document.querySelector('.redact-action').onclick = function() {
-  actionRedact();
+  document.querySelector('.redacted-container textarea').innerText = redactedText;
 };
 
 actionRedact();
+
+// TODO
+// comma seperated terms
+// one click copy redacted output
+// focus on redact input text
